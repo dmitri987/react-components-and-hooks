5fc4c428e4bb2354513a6abb5c340deebc4c2b5e
Dmytro <shorop.d@gmail.com>
2023-03-01 18:45:27 +0200

    hooks: add usePrevious

---
+++ src/hooks/usePrevious/usePrevious.ts
@@ -0,0 +1,8 @@
+import { useRef } from "react";
+
+export default function usePrevious(value: any) {
+  const prevRef = useRef(value);
+  const prev = prevRef.current;
+  if (value !== prev) prevRef.current = value;
+  return prev ?? value;
+}
