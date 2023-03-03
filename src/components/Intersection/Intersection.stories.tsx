ba64dd10857914f2348207bde1e7625d43ba5139
Dmytro <shorop.d@gmail.com>
2023-03-03 21:41:01 +0200

    refactor: ReactiveIntersectionObserver related and stories
    <IntersectionProvider>
      <Intersection>
    
    useIntersectionObserver(options?): ReactiveIntersectionObserver
    useIntersection(options?): { ref?, entry, observer }

--- src/components/Intersection/Intersection.stories.tsx
+++ src/components/Intersection/Intersection.stories.tsx
@@ -2,16 +2,16 @@ import { ComponentMeta } from "@storybook/react";
 import { useCallback, useEffect, useRef, useState } from "react";
 import { Describe } from "../../components/Testing/Describe";
 import "../../index.css";
-import getIntersectionObserver, {
+import {
+  getIntersectionObserver,
   OnIntersectCallback,
   ReactiveIntersectionObserver,
-  ReactiveIntersectionObserverInit,
   resolveRootMargin,
 } from "../../libs/reactive-intersection-observer";
-import useIntersectionObserver from "./useIntersectionObserver";
+import { useIntersection, useIntersectionObserver } from "./Intersection";
 
 export default {
-  title: "hooks/useIntersectionObserver",
+  title: "components/Intersection",
   // component: Button,
   parameters: {
     layout: "padded",
@@ -39,85 +39,145 @@ export default {
  *  </Describe>
  */
 
-const shouldThrow = (fn: Function) => {
-  try {
-    fn();
-  } catch (err) {
-    return true;
-  }
-  return false;
-};
+// const shouldThrow = (fn: Function) => {
+//   try {
+//     fn();
+//   } catch (err) {
+//     return true;
+//   }
+//   return false;
+// };
 
 export const Tests = () => {
   const [container, setContainer] = useState<Element | null>(null);
   const containerRef = useRef<Element | null>(null);
   const container1Ref = useRef(null);
 
-  const [root, setRoot] = useState<Element | Document | null>(null);
-  const [rootMargin, setRootMargin] = useState<string>("");
-  const [threshold, setThreshold] = useState<number | number[]>(0);
+  // const [root, setRoot] = useState<Element | Document | null>(null);
+  // const [rootMargin, setRootMargin] = useState<string>("");
+  // const [threshold, setThreshold] = useState<number | number[]>(0);
 
-  useEffect(() => {
-    setTimeout(() => setRoot(document), 50);
-    setTimeout(() => setRootMargin("200px"), 100);
-    setTimeout(() => setThreshold([0.5, 1]), 150);
-  }, []);
+  // useEffect(() => {
+  //   setTimeout(() => setRoot(document), 50);
+  //   setTimeout(() => setRootMargin("200px"), 100);
+  //   setTimeout(() => setThreshold([0.5, 1]), 150);
+  // }, []);
 
   useEffect(() => {
     containerRef.current = container;
   }, [container]);
 
-  const empty = useIntersectionObserver(container, () => {});
+  const observer1 = useIntersectionObserver();
+  const observer2 = useIntersectionObserver({ rootMargin: "100px" });
+  const observer3 = useIntersectionObserver({ rootMargin: "100px 100px" });
 
+  const targetRef4 = useRef(null);
+  const observer4 = useIntersectionObserver({ root: targetRef4 });
+
+  const [target5, setTarget5] = useState<HTMLDivElement | null>(null);
+  const observer5 = useIntersectionObserver({ root: target5 });
+
+  const observer6 = useIntersectionObserver({ rootMargin: "100px" });
+
+  const result7 = useIntersection();
+  const result8 = useIntersection();
+  const result9 = useIntersection();
+
+  const targetRef10 = useRef(null);
+  const result10 = useIntersection({ target: targetRef10 });
+
+  const [target11, setTarget11] = useState<HTMLDivElement | null>(null);
+  const result11 = useIntersection({ target: target11 });
+
+  const [counter12, setCounter12] = useState(0);
+  const result12 = useIntersection({
+    onIntersect: () => setCounter12((c) => c + 1),
+  });
+
+  const [target13, setTarget13] = useState<HTMLDivElement | null>(null);
+  const [result13, setResult13] = useState("");
+  useIntersection({ target: target13, onIntersect: () => setResult13("A") });
+  useIntersection({ target: target13, onIntersect: () => setResult13("B") });
+
+  const [target14, setTarget14] = useState<HTMLDivElement | null>(null);
+  const [result14, setResult14] = useState("");
   const [onIntersect, setOnIntersect] =
     useState<OnIntersectCallback | null>(null);
-  const [onIntersectResult, setOnIntersectResult] = useState("");
-  const onIntersectB = useCallback(() => setOnIntersectResult("B"), []);
-  const onIntersectA = useCallback(() => setOnIntersectResult("A"), []);
+  const onIntersectB = useCallback(() => setResult14("B"), []);
+  const onIntersectA = useCallback(() => setResult14("A"), []);
 
-  useIntersectionObserver(container, onIntersect ?? onIntersectA);
+  useIntersection({
+    target: target14,
+    onIntersect: onIntersect ?? onIntersectA,
+  });
+
   useEffect(() => {
     setTimeout(() => setOnIntersect(() => onIntersectB), 0);
   }, []);
 
-  const [result1, setResult1] = useState("");
-  useIntersectionObserver(container1Ref, () => setResult1("A"));
-  useIntersectionObserver(container1Ref, () => setResult1("B"));
+  const [target15, setTarget15] = useState<HTMLDivElement | null>(null);
+  const result15 = useIntersection({ target: target15 });
 
-  const container2Ref = useRef<HTMLDivElement | null>(null);
-  const [resultObservers2, setResultObservers2] = useState<
-    ReactiveIntersectionObserver[]
-  >([]);
-  const [options2, setOptions2] = useState<ReactiveIntersectionObserverInit>(
-    {}
-  );
-  useIntersectionObserver(
-    container2Ref,
-    (_, observer) => setResultObservers2((a) => [...a, observer]),
-    options2
-  );
-  useEffect(() => {
-    setTimeout(() => setOptions2({ rootMargin: "200px" }));
-    setTimeout(() => setOptions2({ rootMargin: "400px" }), 10);
-  }, [40]);
+  const [target16, setTarget16] = useState<HTMLDivElement | null>(null);
+  const [toObserve16, setToObserve16] = useState(true);
+  const result16 = useIntersection({ target: toObserve16 && target16 });
 
-  const container3Ref = useRef<HTMLDivElement | null>(null);
-  const container4Ref = useRef<HTMLDivElement | null>(null);
-  const [curTarget, setCurTarget] = useState<Element | null>(null);
-  const [curResultTarget, setCurResultTarget] = useState<Element | null>(null);
-  useIntersectionObserver(curTarget ?? container3Ref, (e) =>
-    setCurResultTarget(e.target)
-  );
   useEffect(() => {
-    setTimeout(() => setCurTarget(container4Ref.current), 50);
+    setTimeout(() => setToObserve16(false), 30);
   }, []);
+
+  const [target17, setTarget17] = useState<HTMLDivElement | null>(null);
+  const [target18, setTarget18] = useState<HTMLDivElement | null>(null);
+  const [curTarget17, setCurTarget17] = useState<HTMLDivElement | null>(null);
+  const result17 = useIntersection({ target: curTarget17 ?? target17 });
+
+  useEffect(() => {
+    setTimeout(() => setCurTarget17(target18), 30);
+  }, [target18]);
+
+  // const container2Ref = useRef<HTMLDivElement | null>(null);
+  // const [resultObservers2, setResultObservers2] = useState<
+  //   ReactiveIntersectionObserver[]
+  // >([]);
+  // const [options2, setOptions2] = useState<ReactiveIntersectionObserverInit>(
+  //   {}
+  // );
+  // useIntersection({
+  //   target: container2Ref,
+  //   onIntersect: (_, observer) => setResultObservers2((a) => [...a, observer]),
+  // });
+  // useEffect(() => {
+  //   setTimeout(() => setOptions2({ rootMargin: "200px" }));
+  //   setTimeout(() => setOptions2({ rootMargin: "400px" }), 10);
+  // }, [40]);
+
+  // const container3Ref = useRef<HTMLDivElement | null>(null);
+  // const container4Ref = useRef<HTMLDivElement | null>(null);
+  // const [curTarget, setCurTarget] = useState<Element | null>(null);
+  // const [curResultTarget, setCurResultTarget] = useState<Element | null>(null);
+  // useIntersection({
+  //   target: curTarget ?? container3Ref,
+  //   onIntersect: (e) => setCurResultTarget(e.target),
+  // });
+  // useEffect(() => {
+  //   setTimeout(() => setCurTarget(container4Ref.current), 50);
+  // }, []);
 
   return (
     <div className="w-full border" ref={setContainer}>
       <div id="container1" ref={container1Ref}></div>
-      <div id="container2" ref={container2Ref}></div>
-      <div id="container3" ref={container3Ref}></div>
-      <div id="container4" ref={container4Ref}></div>
+      <div ref={targetRef4}></div>
+      <div ref={setTarget5}></div>
+      <div ref={result8.ref}></div>
+      <div ref={targetRef10}></div>
+      <div ref={setTarget11}></div>
+      <div ref={result12.ref}></div>
+      <div ref={setTarget13}></div>
+      <div ref={setTarget14}></div>
+      <div ref={setTarget15}></div>
+      <div ref={setTarget16}></div>
+      <div ref={setTarget17} id="target17"></div>
+      <div ref={setTarget18} id="target18"></div>
       <Describe title="getIntersectionObserver(options?: IntersectionObserverInit): void">
         {[
           [
@@ -231,27 +291,85 @@ export const Tests = () => {
           ],
         ]}
       </Describe>
-      <Describe title="useIntersectionObserver(target, onIntersect, options?): void">
+      <Describe title="useIntersectionObserver(options?): ReactiveResizeObserver">
         {[
-          [`should return void`, empty === undefined],
           [
-            `should ignore further changes of 'onIntersect' once it's set`,
-            onIntersectResult === "A",
+            `useIntersectionObserver() === getIntersectionObserver()`,
+            observer1 === getIntersectionObserver(),
           ],
           [
-            `useIntersectionObserver(target, onIntersect1); useIntersectionObserver(target, onIntersect2); should subscribe 'target' with 'onIntersect2'`,
-            result1 === "B",
+            `useIntersectionObserver({ rootMargin: '100px' }) === useIntersectionObserver({ rootMargin: '100px 100px' })`,
+            observer2 === observer3,
           ],
           [
-            `should not recreate observer if 'options' is changed`,
-            resultObservers2.length === 1 ||
-              (resultObservers2.length === 2 &&
-                resultObservers2[0] === resultObservers2[1]),
+            `useIntersectionObserver({ root: rootRef }).root === rootRef.current`,
+            observer4?.root === targetRef4.current,
           ],
           [
-            `should dynamically switch targets`,
-            curResultTarget === container4Ref.current &&
-              curResultTarget !== container3Ref.current,
+            `useIntersectionObserver({ root: rootElem }).root === rootElem`,
+            observer5?.root === target5,
+          ],
+          [
+            `useIntersectionObserver({ rootMargin: '100px' }).rootMargin === '100px 100px 100px 100px'`,
+            observer6?.rootMargin === "100px 100px 100px 100px",
+          ],
+        ]}
+      </Describe>
+
+      <Describe title="useIntersection(options): { ref?, entry, observer }">
+        {[
+          [
+            `useIntersection() should return { ref: (instance: Element) => void }`,
+            result7?.ref instanceof Function && result7?.ref.length == 1,
+          ],
+          [
+            `const { ref, entry } = useIntersection() should return null 'entry' if 'ref' isn't bound to a target element`,
+            result7?.entry === null,
+          ],
+          [
+            `const { ref, entry } = useIntersection() should return non-null 'entry' if 'ref' is bound to a target element`,
+            result8?.entry !== null && result8?.entry.target instanceof Element,
+          ],
+          [
+            `const { observer } = useIntersection(); observer === getIntersectionObserver()`,
+            result9?.observer === getIntersectionObserver(),
+          ],
+          [
+            `useIntersection({ target: elem }) should not return { ref }`,
+            result10?.ref === undefined,
+          ],
+          [
+            `const { entry } = useIntersection({ target: elem }); entry.target === elem`,
+            !!result11?.entry && result11?.entry.target === target11,
+          ],
+          [
+            `useIntersection({ onIntersect: () => ... }) should call onIntersect on every intersection`,
+            counter12 > 0,
+          ],
+          [
+            `useIntersection({ onIntersect: onIntersectA }); useIntersection({ onIntersect: onIntersectB }); should change onIntersect to onIntersectB`,
+            result13 === "B",
+          ],
+          [
+            `useIntersection({ onIntersect }) should remember 'onIntersect' on first call and it should not be dynamically changed`,
+            result14 === "A",
+          ],
+          [
+            `const { observer } = useIntersection({ target: elem }); observer.targets should contain 'elem'`,
+            !!target15 && result15?.observer?.targets.includes(target15),
+          ],
+          [
+            `useIntersection({ target: false && element }) should unobserve target`,
+            !!target16 && !result16.observer?.targets.includes(target16),
+          ],
+          [
+            `useIntersection({ target }) 'target' should be dynamically changed`,
+            !!target17 &&
+              !!target18 &&
+              !!result17.observer &&
+              result17.observer?.targets.includes(target18) &&
+              !result17.observer.targets.includes(target17) &&
+              result17.entry?.target === target18,
           ],
         ]}
       </Describe>
@@ -363,14 +481,10 @@ export const Tests = () => {
   );
 };
 
-export const UseIntersectionObserver = () => {
+export const UseIntersection = () => {
   const [targetA, setTargetA] = useState<Element | null>(null);
   const [targetB, setTargetB] = useState<Element | null>(null);
-  const [intersectionRatioA, setIntersectionRatioA] = useState<number>(0);
-  const [intersectionRatioB, setIntersectionRatioB] = useState<number>(0);
   const [target, setTarget] = useState<Element | null>(null);
-  const [container, setContainer] = useState<Element | null>(null);
-  const [rootMargin, setRootMargin] = useState("0px");
   const [observer, setObserver] =
     useState<ReactiveIntersectionObserver | null>(null);
 
@@ -378,30 +492,28 @@ export const UseIntersectionObserver = () => {
     if (targetA) setTarget(targetA);
   }, [targetA]);
 
-  const onIntersectA = useCallback(
+  const onIntersect = useCallback(
     (
       entry: IntersectionObserverEntry,
       observer: ReactiveIntersectionObserver
     ) => {
       if (!entry) return;
 
-      entry.target.textContent =
-        "onIntersectA: " + entry.intersectionRatio.toString();
+      entry.target.textContent = entry.intersectionRatio.toString();
       setObserver(observer);
     },
     []
   );
 
-  const [onIntersect, setOnIntersect] =
-    useState<OnIntersectCallback | null>(null);
-
-  useIntersectionObserver(target, onIntersect ?? onIntersectA, {
-    rootMargin,
+  useIntersection({
+    target,
+    onIntersect,
+    rootMargin: '0px',
     threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
   });
 
   return (
-    <div className="w-full border" ref={setContainer}>
+    <div className="w-full border">
       <button
         className="border rounded p-2"
         onClick={() => {
@@ -426,10 +538,8 @@ export const UseIntersectionObserver = () => {
       )}
       <div className="flex h-[80vh] w-full gap-4">
         <div ref={setTargetA} className="w-full h-full border rounded">
-          A: {intersectionRatioA}
         </div>
         <div ref={setTargetB} className="w-full h-[50%] border rounded">
-          B: {intersectionRatioB}
         </div>
       </div>
     </div>
---
+++ src/components/Intersection/Intersection.tsx
@@ -0,0 +1,165 @@
+import {
+  createContext,
+  ReactNode,
+  RefObject,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
+import {
+  getIntersectionObserver,
+  OnIntersectCallback,
+  ReactiveIntersectionObserver,
+  ReactiveIntersectionObserverInit,
+} from "../../libs/reactive-intersection-observer";
+
+const IntersectionObserverContext =
+  createContext<ReactiveIntersectionObserver | null | undefined>(undefined);
+
+export type IntersectionProviderProps = {
+  options?: ReactiveIntersectionObserverInit;
+  children?: ReactNode;
+};
+
+export const IntersectionProvider = ({
+  options,
+  children,
+}: IntersectionProviderProps) => {
+  const observer = useIntersectionObserver(options);
+  return (
+    <IntersectionObserverContext.Provider value={observer}>
+      {children}
+    </IntersectionObserverContext.Provider>
+  );
+};
+
+/*
+ * Return null for observer, which 'root' is waiting
+ * to be resolved from null to Element
+ *
+ * Priority:
+ *   observer based on 'options' argument ->
+ *   observer provided by parent <IntersectionProvider> ->
+ *   default observer
+ */
+export function useIntersectionObserver(
+  options?: ReactiveIntersectionObserverInit
+): ReactiveIntersectionObserver | null {
+  const observerContext = useContext(IntersectionObserverContext);
+  // console.log("useIntersectionObserver:", observerContext, options);
+  return useMemo(
+    () =>
+      !options?.root && observerContext !== undefined
+        ? observerContext
+        : getIntersectionObserver(options),
+    [options]
+  );
+}
+
+/*
+ * const { ref, entry, observer } = useIntersection();
+ * const { ref, entry, observer } = useIntersection({
+ *   root: rootRef,
+ *   rootMargin: '20vh 20vw 100px 50%',
+ *   threshold: [0, 0.5, 1],
+ *   onIntersect: (entry => console.log(entry)),
+ * });
+ *    ...
+ *  <div ref={ref}>...</div>
+ *
+ *
+ * const targetRef = useRef(null);
+ * const { entry, observer } = useIntersection({ target: toObserve && targetRef });
+ *    ...
+ *  <div ref={targetRef}
+ *
+ */
+
+export type IntersectionOptions = {
+  onIntersect?: OnIntersectCallback;
+  target?: Element | RefObject<Element> | false | null;
+} & ReactiveIntersectionObserverInit;
+
+export type IntersectionReturnType = {
+  ref?: (instance: Element | null) => void;
+  entry: IntersectionObserverEntry | null;
+  observer: ReactiveIntersectionObserver | null;
+};
+
+const observerOptions = (options?: IntersectionOptions) => {
+  const { root, rootMargin, threshold } = options ?? {};
+  if (root !== undefined || rootMargin !== undefined || threshold !== undefined)
+    return {
+      root,
+      rootMargin,
+      threshold,
+    };
+
+  return undefined;
+};
+export function useIntersection(
+  options?: IntersectionOptions
+): IntersectionReturnType {
+  const [entry, setEntry] = useState<IntersectionObserverEntry | null>(null);
+  const [target, setTarget] =
+    useState<Element | RefObject<Element> | null>(null);
+  const { target: _target, onIntersect } = options ?? {};
+  const onIntersectRef = useRef(onIntersect);
+
+  const observer = useIntersectionObserver(observerOptions(options));
+
+  const setRef = useCallback((instance: Element | null) => {
+    setTarget(instance);
+  }, []);
+
+  useEffect(() => {
+    if (_target === undefined) return;
+console.log('useIntersection: _target', _target)
+    setTarget(_target || null);
+  }, [_target]);
+
+  useEffect(() => {
+    if (!observer || !target) return;
+
+    console.log("Intersection init", target, observer);
+
+    observer.observe(target, (entry) => {
+      if (onIntersectRef.current) onIntersectRef.current(entry, observer);
+      setEntry(entry);
+    });
+    return () => observer.unobserve(target);
+  }, [target, observer]);
+
+  const result: IntersectionReturnType = { entry, observer };
+  if (_target === undefined) result.ref = setRef;
+
+  return result;
+}
+
+export type IntersectionProps = {
+  options?: IntersectionOptions;
+  className?: string;
+  children?:
+    | ReactNode
+    | ((
+        entry: IntersectionObserverEntry | null,
+        observer: ReactiveIntersectionObserver | null
+      ) => ReactNode);
+};
+
+export const Intersection = ({
+  options,
+  className,
+  children,
+}: IntersectionProps) => {
+  const { ref, entry, observer } = useIntersection(options);
+
+  return (
+    <div ref={ref} className={className}>
+      {children instanceof Function ? children(entry, observer) : children}
+    </div>
+  );
+};
