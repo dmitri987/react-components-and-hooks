78a4d76896c2186d7e04afbf01250752e92600bf
Dmytro <shorop.d@gmail.com>
2023-03-06 00:49:05 +0200

    hooks: add 'enable' to useSum

--- src/hooks/useSum/useSum.ts
+++ src/hooks/useSum/useSum.ts
@@ -4,22 +4,28 @@ export type UseSumReturnType = [number, (v: number) => void];
 
 export default function useSum(
   value: number,
-  clamp?: [min: number, max: number]
+  options?: {
+    clamp?: [min: number, max?: number];
+    enable?: boolean;
+  }
 ): UseSumReturnType {
   const [, triggerRender] = useState(false);
-
   const sumRef = useRef(0);
-  sumRef.current = !clamp
-    ? sumRef.current + value
-    : Math.min(
-        Math.max(sumRef.current + value, clamp[0] ?? -Infinity),
-        clamp[1] ?? +Infinity
-      );
-
   const setSum = useCallback((newValue: number) => {
     sumRef.current = newValue;
     triggerRender((t) => !t);
   }, []);
+
+  const { enable, clamp } = options ?? {};
+
+  if (enable !== false) {
+    sumRef.current = !clamp
+      ? sumRef.current + value
+      : Math.min(
+          Math.max(sumRef.current + value, clamp[0] ?? -Infinity),
+          clamp[1] ?? +Infinity
+        );
+  }
 
   return [sumRef.current, setSum];
 }
